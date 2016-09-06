cd1791a528c0585b8e5a433d347c30a25f8ce256
Danielle Tomlinson <dan@tomlinson.io>
2016-09-06 22:48:02 +0200

    This is terrible

--- lib/theobroma.rb
+++ lib/theobroma.rb
@@ -1,5 +1,15 @@
 require "theobroma/version"
 
 module Theobroma
-  # Your code goes here...
+  require 'theobroma/environment'
+  require 'theobroma/installer'
+  require 'theobroma/parser'
+
+  def self.setup
+    load File.expand_path('../theobroma/setup.rb', __FILE__)
+  end
+
+  def self.cocoapods_version
+    defined?(CocoaPods::VERSION) && CocoaPods::VERSION
+  end
 end
